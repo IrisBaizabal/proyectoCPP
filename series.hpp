8fcb7d49ecc4d05789678a2b9ad8d9537305830b
Francisco <A00843605@tec.mx>
2026-06-03 13:17:47 -0600

    cambio avance 1

--- series.hpp
+++ series.hpp
@@ -1,1 +1,14 @@
-// series.hpp
+#ifndef series_hpp
+#define series_hpp
+
+#include video.hpp  //to inherit video 
+#include episode.hpp //inherit episode
+#include <iostream>
+#include <vector>
+
+class Series: public Video{
+    private:
+        
+}
+
+#endif
